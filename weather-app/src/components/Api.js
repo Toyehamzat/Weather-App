94a05ef1805e4571e5d3ae69f5a0909a645a064a
toye <anibiseun@gmail.com>
2023-04-02 10:10:50 +0100

    added Api for the search component

---
+++ weather-app/src/components/Api.js
@@ -0,0 +1,8 @@
+export const getApiOrptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '548e79e095msh30b1792f292a1fdp13f46ejsn64e7cdfa6822',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
---
+++ weather-app/src/components/current-weather/current-weather.js
@@ -0,0 +1,15 @@
+import "./current-weather.css"
+import React from "react"
+
+function CurrentWeather(){
+    return(
+        <div className="weather">
+            <div className="top">
+                <p className="city">lagos</p>
+                <p className="weather-description">sunny</p>
+            </div>
+        </div>
+    )
+
+}
+export default CurrentWeather;
