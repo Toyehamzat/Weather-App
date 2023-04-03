5bf9624e49b27a3983e9bf7fc0ca5795bb8d7b29
sijibomi <gbemilesanmi@gmail.com>
2023-04-03 21:17:45 +0100

    fix: weatherapp, default initial value is lagos

--- src/App.js
+++ src/App.js
@@ -2,34 +2,16 @@ import "./App.css";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "../src/components/Api";
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import Forecast from "./components/forecast/forecast";
+
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
 
   const [forecast, setForecast] = useState(null);
 
-  // const defaultSearch=(Search)=>{
-  //   // const [lat, lon] = Search.value.split(" ");
-  //   const CurrentWeatherFetch = fetch(
-  //     `${WEATHER_API_URL}/weather?lat=6.45}&lon=3.4appid=${WEATHER_API_KEY}&units=metric`
-  //   );
-  //   const forcastFetch = fetch(
-  //     `${WEATHER_API_URL}/forecast?lat=6.45&lon=3.4&appid=${WEATHER_API_KEY}&units=metric`
-  //   );
+    const defaultValue = { value:"6.52, 3.37" , label:"Lagos,NG" }
 
-  //   Promise.all([CurrentWeatherFetch, forcastFetch])
-  //     .then(async (response) => {
-  //       const weatherResponse = await response[0].json();
-  //       const forcastResponse = await response[1].json();
-
-  //       setCurrentWeather({ city: Search.label, ...weatherResponse });
-  //       setForecast({ city: Search.label, ...forcastResponse });
-  //     })
-  //     .catch(console.log);
-  
-
-  // };
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
     const CurrentWeatherFetch = fetch(
@@ -49,6 +31,11 @@ function App() {
       })
       .catch(console.log);
   };
+
+    useEffect(() => {
+        handleOnSearchChange(defaultValue)
+    }, [])
+
   console.log(currentWeather);
   console.log(forecast);
   return (
